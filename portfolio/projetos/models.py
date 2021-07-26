68eb74c34becea4dcb72dce3fd1da0f71a6fd83d
Marcos Fabricio Sizanosky <fabricio_sizanosky@hotmail.com>
2021-07-26 03:40:41 -0300

    Add banco de dados e user admin

--- portfolio/projetos/models.py
+++ portfolio/projetos/models.py
@@ -1,3 +1,18 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
+
+
+class Projeto(models.Model):
+    """
+    Classe para modelar e criar banco de dados de projetos para um APP
+    """
+    nome_projeto = models.CharField(max_length=200)
+    descricao_projeto = models.TextField()
+    cliente = models.CharField(max_length=100)
+    data_conclusao = models.DateTimeField(default=datetime.now, blank=True)
+    website = models.CharField(max_length=200)
+    linguagem = models.CharField(max_length=100)
+    tags_projeto = models.TextField()
+    detahes_projeto = models.TextField()
--- portfolio/projetos/views.py
+++ portfolio/projetos/views.py
@@ -9,8 +9,16 @@ def index(request):
 
 
 def projetos(request):
+    dic_projetos = {
+        'proj_1': 'Weather Around World - usando API',
+        'proj_2': "Buscando noticias com API's",
+        'proj_3': 'Logando no Facebook - usando Selenium'
+    }
+    dados_projetos = {
+        'nome_projetos': dic_projetos
+    }
     return render(request,
-                  'projetos/projetos.html')
+                  'projetos/projetos.html', dados_projetos)
 
 
 def projeto_details(request):
